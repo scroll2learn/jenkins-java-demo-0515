4994b76e5f4bc2f80adbb2b2381010271e4d1c34
scroll2learn <73377680+scroll2learn@users.noreply.github.com>
2026-05-15 18:04:07 +0530

    Update App.java

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -5,7 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Application started successfully!");
         System.out.println("Welcome to Jenkins CI/CD Demo");
-        System.out.println("New change deployed using Jenkins");
+        System.out.println("New change deployed using Jenkins.");
     }
 
     public static int add(int a, int b) {
